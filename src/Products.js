e385af1f42728e99497e663c2c1a3a66445a11bb
Thoss24 <t.fogarty24@outlook.com>
2023-07-06 20:52:10 +0100

    loaded fetch results

--- src/Products.js
+++ src/Products.js
@@ -1,33 +1,48 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import CartContext from "./components/store/cart-context";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
   const ctx = useContext(CartContext);
 
   const fetchItemsRequest = async () => {
     // https://fakestoreapi.com/products
 
     try {
-        const response = await fetch('https://fakestoreapi.com/products');
+      const response = await fetch("https://fakestoreapi.com/products");
 
-        if (!response.ok) {
-            throw new Error("Something went wrong");
-        }
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
 
-        const data = response.json()
-        console.log(data)
+      const data = await response.json();
 
-        
-    }   catch (err) {
-        console.log(err.message)
+      const loadedData = [];
+
+      for (const key in data) {
+        loadedData.push({
+          title: data[key].title,
+          description: data[key].description,
+          price: data[key].price,
+          id: data[key].id,
+        });
+      }
+
+      setProducts(loadedData);
+    } catch (err) {
+      console.log(err.message);
     }
+  };
 
-
+  const buttonTest = () => {
+    console.log(products);
   };
 
   return (
     <div>
       <h1>Hello from Products</h1>
+      <button onClick={fetchItemsRequest}>FETCH</button>
+      <button onClick={buttonTest}>TEST ARR</button>
     </div>
   );
 };
